36abdc5065cf43ce8a29ad1c5e7856566504f005
cwallz <108586546+cwallz@users.noreply.github.com>
2022-11-15 17:45:23 -0300

    Implementando os controllers - Spring boot

--- codigoFonte/src/main/java/com/example/demo/repositories/AlunoRepository.java
+++ codigoFonte/src/main/java/com/example/demo/repositories/AlunoRepository.java
@@ -1,9 +1,18 @@
 package com.example.demo.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entities.Aluno;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long>{
 
+	@Modifying
+    @Transactional
+    @Query(value = "Update tb_aluno set nome = :#{#aluno.nome} WHERE id = :#{#aluno.id} " , nativeQuery = true)
+	void atualizarAluno(@Param("aluno") Aluno aluno);
 }
